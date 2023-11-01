3f4a6b802bbb20b9d3a91f7b9f6aa728df03d202
Pol556 <21-30169@g.batstate-u.edu.ph>
2023-11-01 18:51:01 +0800

    database

---
+++ database/db_nt3101.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2023 at 11:44 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_nt3101`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admindb`
+--
+
+CREATE TABLE `admindb` (
+  `adminID` int(11) NOT NULL,
+  `password` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdb`
+--
+
+CREATE TABLE `orderdb` (
+  `OrderID` int(100) NOT NULL,
+  `ProductID` int(100) NOT NULL,
+  `SR-Code` varchar(100) NOT NULL,
+  `Quantity` int(20) NOT NULL,
+  `Price` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productdb`
+--
+
+CREATE TABLE `productdb` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(255) NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `productdb`
+--
+
+INSERT INTO `productdb` (`ProductID`, `ProductName`, `Price`, `image`) VALUES
+(1, 'Shot Glass', '9', '320140-removebg-preview.png'),
+(3, 'Pin', '4', 'IMS.png'),
+(4, 'Shirt', '200', 'EscuzarPaulAlvin_BSIT2203.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopcart`
+--
+
+CREATE TABLE `shopcart` (
+  `CartID` int(11) NOT NULL,
+  `SR-Code` varchar(255) NOT NULL,
+  `ProductID` int(100) NOT NULL,
+  `Quantity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_record`
+--
+
+CREATE TABLE `student_record` (
+  `SR_Code` varchar(100) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `dept` varchar(255) NOT NULL,
+  `prog_sec` varchar(255) NOT NULL,
+  `cnum` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_record`
+--
+
+INSERT INTO `student_record` (`SR_Code`, `pass`, `firstname`, `lastname`, `email`, `dept`, `prog_sec`, `cnum`) VALUES
+('21-30169', 'Pol', 'Paul Alvin ', 'Escuzar', '21-30169@g.batstate-u.edu.ph', 'CICS', 'BSIT-NT-3101', '09090774577'),
+('21-34945', 'winforyou', 'Wingell Lord', 'Vinas', '21-34945@g.batstate-u.edu.ph', 'CICS', 'BSIT-NT-3101', '09445278541'),
+('21-55575', 'Marie', 'Marie Glen', 'Cuenca', '21-55575@g.batstate-u.ph', 'CCS', 'BCSC-3e', '0977123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trackorder`
+--
+
+CREATE TABLE `trackorder` (
+  `TrackID` int(100) NOT NULL,
+  `OrderID` int(100) NOT NULL,
+  `SR-Code` varchar(100) NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  `Fee` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admindb`
+--
+ALTER TABLE `admindb`
+  ADD PRIMARY KEY (`adminID`);
+
+--
+-- Indexes for table `orderdb`
+--
+ALTER TABLE `orderdb`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `productdb`
+--
+ALTER TABLE `productdb`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Indexes for table `shopcart`
+--
+ALTER TABLE `shopcart`
+  ADD PRIMARY KEY (`CartID`);
+
+--
+-- Indexes for table `student_record`
+--
+ALTER TABLE `student_record`
+  ADD PRIMARY KEY (`SR_Code`);
+
+--
+-- Indexes for table `trackorder`
+--
+ALTER TABLE `trackorder`
+  ADD PRIMARY KEY (`TrackID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orderdb`
+--
+ALTER TABLE `orderdb`
+  MODIFY `OrderID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `productdb`
+--
+ALTER TABLE `productdb`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `shopcart`
+--
+ALTER TABLE `shopcart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trackorder`
+--
+ALTER TABLE `trackorder`
+  MODIFY `TrackID` int(100) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
